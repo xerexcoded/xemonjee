3d3d161b65700ca4027ad647bf73c10e6ecdcf0b
xerexcoded <arnavbanerjee61@gmail.com>
2021-11-18 13:47:19 +0530

    almost there

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use std::collections::HashMap;
 
 enum Command {
-    SetVar(String , Value),
+    SetVar(String, Value),
     Getvar(String),
     PushVar(String),
     Push(Value),
     Pop,
-    Add
+    Add,
 }
 
-#[derive(Clone , Debug , PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 enum Value {
     Nothing,
-    Int(64),
+    Int(i64),
     String(String),
 }
 
-#[derive(Clone , Debug , PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 enum Type {
     Nothing,
     Int,
@@ -33,46 +33,71 @@ enum EvalandParsingError {
 }
 
 struct Evaluator {
+    vars: HashMap<String, Value>,
+    stack: Vec<Value>,
+}
+impl Evaluator {
     fn new() -> Evaluator {
         Self {
-            vars : HashMap::new(),
+            vars: HashMap::new(),
             stack: vec![],
         }
     }
     fn pop(&mut self) -> Result<Value, EvalandParsingError> {
-        let result = self.stack.pop(); // poping from the stack 
+        let result = self.stack.pop(); // poping from the stack
         match result {
-            Some(V) => Ok(v),
+            Some(v) => Ok(v),
             None => return Err(EvalandParsingError::EmptyStack),
         }
     }
-    fn add(&self, lhs: value ,rhs:value)-> Result<Value, EvalandParsingError>{
-        match(lhs,rhs) {
-            (Value::Int(i1),Value::Int(i2)) => Ok(Value::Int(i1+i2)),
-            (Value::String(s1),Value::String(s2)) => Ok(Value:String(s1+ &s2)),
+    fn add(&self, lhs: value, rhs: value) -> Result<Value, EvalandParsingError> {
+        match (lhs, rhs) {
+            (Value::Int(i1), Value::Int(i2)) => Ok(Value::Int(i1 + i2)),
+            (Value::String(s1), Value::String(s2)) => Ok(Value::String(s1 + &s2)),
             _ => Err(EvalandParsingError::MismatchType),
         }
     }
-    fn 
-    
+    fn evaluate(&mut self, commands: &[Commands]) -> Result<Value, EvalandParsingError> {
+        let mut output = Ok(Value::Nothing); //initial state
+        for command in commands {
+            match command {
+                Command::SetVar(name, value) => {
+                    self.vars.insert(name.into(), value.clone());
+                }
+                Command::GetVar(name) => match self.vars.get(name) {
+                    Some(value) => output = Ok(value.clone()),
+                    None => return Err(EvalandParsingError::MissingVariable(name.into())),
+                },
+                Command::PushVar(name) => match self.vars.get(name) {
+                    Some(value) => self.stack.push(value.clone()),
+                    None => return Err(EvalandParsingError::MissingVariable(name.into())),
+                },
+                Command::Push(v) => self.stack.push(v.clone()),
+                Command::Pop => {
+                    output = self.pop();
+                }
+                Command::Add => {
+                    let lhs = self.pop()?;
+                    let rhs = self.pop()?;
+
+                    let result = self.add(lhs, rhs)?;
+                    self.stack.push(result)
+                }
+            }
+        }
+        output
+    }
 }
 
-
-fn main() {
-    
+fn parse_var_name(var_name: &str) -> Result<String, EvalandParsingError> {
+    Ok(var_name.into())
+}
+fn parse_string(val: &str) -> Result<Value, EvalandParsingError> {
+    if val.starts_with("\"") && val.ends_with("\"") && val.len() > 1 {
+        //>1 as to  avoid parsing quotes singular
+        let inner = val[1..(val.len() - 1)].to_string();
+        Ok(Value::String(inner))
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fn main() {}
